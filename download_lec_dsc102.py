28d5c53a8dd052369ae81bc29dc97c6d745531c1
Xuewen Yang <84886359+daphneyyy@users.noreply.github.com>
2023-04-30 19:42:39 -0700

    Add files via upload

---
+++ download_lec_dsc102.py
@@ -0,0 +1,28 @@
+import requests
+import os
+from bs4 import BeautifulSoup
+
+# make a request to the website
+url = 'https://rod-albuyeh-labs.github.io/dsc102-sp23/'
+response = requests.get(url)
+
+# path of the folder to save the pdfs
+file_path = '/Users/daphneyang/Desktop/DSC102/Lectures/'
+
+# parse the HTML response with Beautiful Soup
+soup = BeautifulSoup(response.text, 'html.parser')
+
+lecs = soup.findAll('strong', class_="label label-lecture")
+for lec in lecs:
+    link_component = lec.parent.find('a')
+    link = link_component.get('href')
+
+    lecture_title = file_path + ('DSC102_' + lec.text.strip() + ' ' +
+                                 link_component.text).strip().replace(':', '')\
+        .replace(' ', '_') + '.pdf'
+
+    if not os.path.exists(lecture_title):
+        pdf_link = url + link
+        pdf = requests.get(pdf_link)
+        with open(lecture_title, 'wb') as f:
+            f.write(pdf.content)
---
+++ download_lec_dsc140b.py
@@ -0,0 +1,35 @@
+import requests
+import os
+from bs4 import BeautifulSoup
+
+# make a request to the website
+url = 'http://dsc140b.com/index.html'
+response = requests.get(url)
+
+# path of the folder to save the pdfs
+file_path = '/Users/daphneyang/Desktop/DSC140B/Lectures/'
+
+# parse the HTML response with Beautiful Soup
+soup = BeautifulSoup(response.text, 'html.parser')
+
+sections = soup.findAll('h3', class_="schedule-week-component-title")
+for sec in sections:
+    if sec.text.strip() == "Dr. Eldridge's Lectures":
+        all_list = sec.parent.findAll('li', class_='schedule-resource')
+        for li in all_list:
+            links = li.findAll('a')
+            for link in links:
+                pdf_link = link.get('href')
+                if pdf_link.split('/')[-1] == 'marked.pdf':
+                    lecture_title = ' '.join([s.strip()
+                                             for s in li.span.strings]).strip()
+                    lecture_title = lecture_title + '.pdf'
+                    lecture_title = file_path + \
+                        lecture_title.replace(
+                            ' ', '_').replace('Lecture', 'LEC')
+
+                    if not os.path.exists(lecture_title):
+                        pdf_link = 'http://dsc140b.com/' + pdf_link
+                        pdf = requests.get(pdf_link)
+                        with open(lecture_title, 'wb') as f:
+                            f.write(pdf.content)
